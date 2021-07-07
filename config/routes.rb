b4adc0db6c23c91a374e6010d672f0e596b9d157
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-07-07 10:04:59 +0000

    [add]ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  get "/" => "homes#top", as: "top"
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :relationships, only: [:create, :destroy]
+
+  resources :notifications, only: [:index]
+  delete "notifications" => "notifications#destroy_all", as: "user_notifications_destroy_all"
+
 end
