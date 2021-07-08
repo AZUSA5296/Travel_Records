0466a7732fb30bb5a050d328bab72d5bdf21c18a
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-07-08 16:40:53 +0000

    [add]画像投稿機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,20 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :baria_user, { only: [:edit, :update, :destroy] }
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "スポットを投稿しました"
+      redirect_to post_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,6 +30,19 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post_image).permit(:title, :date, :image, :content)
+  end
+
+  def baria_user
+    if current_user.nil? || Spot.find(params[:id]).user.id.to_i != current_user.id
+      flash[:alert] = "権限がありません"
+      redirect_to top_path
+    end
   end
 
 end
