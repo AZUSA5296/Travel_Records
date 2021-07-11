91d9d9bcf96edef419f753a0e1d3c8a86342c337
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-07-11 07:21:26 +0000

    [update]コメント機能の非同期通信実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,16 +3,16 @@ class CommentsController < ApplicationController
   before_action :baria_user, only: [:destroy]
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
   end
 
   def destroy
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+    comment.destroy
   end
 
   private
