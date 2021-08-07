0a154bfe52a2700f79bb395a6d56fbb53bedae78
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-08-07 10:16:59 +0000

    [update]user-editレスポンシブ対応

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,6 +39,16 @@ $(function(){
 });
 });
 
+$(function(){
+    $('#user_profile_image').on('change', function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $(".profile_image").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+});
+});
+
 //フラッシュメッセージ
 $(function(){
   setTimeout("$('#notice, #alert').fadeOut('slow')", 1000);
