5cc0b2a8f1e5432e19edbb60dd1f97fe17c3a78d
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-08-24 13:14:39 +0000

    [add]Rspec_postアソシエーション追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -43,6 +43,32 @@ RSpec.describe 'Postモデルのテスト', type: :model do
         is_expected.to eq false
       end
     end
+  end
+
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+
+    context 'Commentモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:comments).macro).to eq :has_many
+      end
+    end
+
+    context 'Favoriteモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:favorites).macro).to eq :has_many
+      end
+    end
+
+    context 'Notificationモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(Post.reflect_on_association(:notifications).macro).to eq :has_many
+      end
+    end
 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Userモデルのテスト', type: :model do
+
   describe 'バリデーションのテスト' do
     subject { user.valid? }
 
@@ -62,8 +63,9 @@ RSpec.describe 'Userモデルのテスト', type: :model do
         is_expected.to eq false
       end
     end
+  end
 
-     describe 'アソシエーションのテスト' do
+  describe 'アソシエーションのテスト' do
     context 'Postモデルとの関係' do
       it '1:Nとなっている' do
         expect(User.reflect_on_association(:posts).macro).to eq :has_many
@@ -100,7 +102,6 @@ RSpec.describe 'Userモデルのテスト', type: :model do
       end
     end
   end
-
   describe 'following, followers関連のテスト' do
     let(:user) { create(:user) }
     let(:other_user) { create(:other_user) }
@@ -114,5 +115,5 @@ RSpec.describe 'Userモデルのテスト', type: :model do
       expect(user.following?(other_user)).to eq false
     end
   end
-  end
+
 end
