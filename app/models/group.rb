10f71dc4a6a10e79e0eb9de2ab25de1f14e34ca3
AZUSA5296 <nnfnnfnakuyo731@gmail.com>
2021-09-02 18:11:04 +0000

    [add]グループ一覧画面

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,6 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
-  validates :introduction, presence: true, length: { maximum: 1000 }
+  validates :introduction, length: { maximum: 1000 }
   attachment :image, destroy: false
 end
